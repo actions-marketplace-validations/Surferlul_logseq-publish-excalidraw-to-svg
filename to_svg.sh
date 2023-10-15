30fbd9862ef07cbf6096c3e4bbb256fc8f2f51b4
Lu Baumann <mail@lu-dev.de>
2023-10-15 23:53:33 +0200

    added debug to to_svg.sh

--- to_svg.sh
+++ to_svg.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo ::debug::'$1:' "$1"
+echo ::debug::'$2:' "$2"
 EXCALIDRAW=$(jq '{"type": .type, "version": .version, "source": .source, "elements": .elements, "appState": {"gridSize": null, "exportWithDarkMode": '"$2"'}}' "$1")
+echo ::debug::'$EXCALIDRAW' "$EXCALIDRAW"
 curl localhost:8080/excalidraw/svg --data-raw "$EXCALIDRAW" > "$1.svg"
