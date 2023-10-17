9518372418551bec0008fc1daab30749d211a52c
Lu Baumann <mail@lu-dev.de>
2023-10-17 02:07:38 +0000

    add svgs to assets/autogen

--- to_svg.sh
+++ to_svg.sh
@@ -3,4 +3,5 @@ EXCALIDRAW="$(jq '{"type": .type, "version": .version, "source": .source, "eleme
 ENCODED="$(echo $EXCALIDRAW | python -c "import sys; import base64; import zlib; print(base64.urlsafe_b64encode(zlib.compress(sys.stdin.read().encode('utf-8'), 9)).decode('ascii'))")"
 echo ::debug::'$ENCODED' "$ENCODED"
 docker ps
-curl "localhost:8080/excalidraw/svg/$ENCODED" > "$1.svg"
+mkdir -p "assets/autogen/$(dirname $1)"
+curl "localhost:8080/excalidraw/svg/$ENCODED" > "assets/autogen/$1.svg"
